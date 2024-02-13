c04e95c98f28ea27f1a20ca7d1c453da9a893af0
Tes R <tesryu39@gmail.com>
2024-02-13 14:36:40 +0000

    Add a meta class for ordering of the post

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,9 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
