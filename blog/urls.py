014e0257b6f092f01ee779c6e820e8139cc1635a
Tes R <tesryu39@gmail.com>
2024-02-15 19:46:17 +0000

    Add About app

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path("<int:event_id>/", views.event_detail,
-         name="event_detail") 
+    path('<slug:slug>/', views.post_detail, name="post_detail"),
 ]
