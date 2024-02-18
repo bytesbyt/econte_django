c711d161fbc8eef97a85c50e66270ffe3c8be28c
Tes R <tesryu39@gmail.com>
2024-02-18 19:16:06 +0000

    Add contact Request form

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, ContactRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Contact
+from .forms import ContactForm
 
 # Create your views here.
 def contact_me(request):
@@ -7,9 +8,13 @@ def contact_me(request):
     Renders the Contact page
     """
     contact = Contact.objects.all().order_by('-updated_on').first()
+    contact_form = ContactForm()
 
     return render(
         request,
         "contact/contact.html",
-        {"contact": contact},
+        {
+            "contact": contact,
+            "contact_form": contact_form
+            },
     )
