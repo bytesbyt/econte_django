8bc3854093e378eae030e8461fcae9b86f5ee446
Tes R <tesryu39@gmail.com>
2024-02-18 19:02:27 +0000

    Create and migrate contact request model

--- contact/models.py
+++ contact/models.py
@@ -8,3 +8,12 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.title
+
+class ContactRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Contact request from {self.name}"
